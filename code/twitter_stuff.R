954f780dde7908b74b2d90da75ac7c367f109653
Auburngrads <auburngrads@live.com>
2017-10-10 16:43:54 -0400

    Update code/twitter_stuff.R

--- code/twitter_stuff.R
+++ code/twitter_stuff.R
@@ -1,22 +1,28 @@
+install.packages(c('twitteR', 'httr', 'lubridate','ggmap'))
+
 library(twitteR)
 library(httr)
 library(lubridate)
 library(ggmap)
 
-setup_twitter_oauth(consumer_key    = jkf::key_chain('twitter.api'),
-                    consumer_secret = jkf::key_chain('twitter.api.secret'),
-                    access_token    = jkf::key_chain('twitter.token'),
-                    access_secret   = jkf::key_chain('twitter.token.secret'))
+setup_twitter_oauth(consumer_key    = twitter_consumer_key,
+                    consumer_secret = twitter_consumer_secret,
+                    access_token    = twitter_access_token,
+                    access_secret   = twitter_access_secret)
 
 tweets <- searchTwitter('#gopats', 
                         geocode = '42.375,-71.1061111,10mi')
 
-lat_lon_dis <- function(location = 'Dayton, OH', distance = '20mi') {
+lat_lon_dis <- function(location = 'Mexico City', distance = '20mi') {
   
   tw_ll <- ggmap::geocode(location)
   paste(c(tw_ll[2], tw_ll[1], distance), collapse = ',')
   
 } 
+
+tweets2 <- searchTwitter('#gopats', 
+                        geocode = mexico)
+
 
 tw_lat_long
 # Convert List of tweets to data.frame
@@ -29,7 +35,7 @@ View(tweet_df)
 check_tweet <- function(tweet) {
 
   id <- tweet$id
-browseURL(paste0('https://twitter.com/statuses/',id)
+browseURL(paste0('https://twitter.com/statuses/',id))
           
 }
 getTimeOfTweets <- function(hashtag, 
